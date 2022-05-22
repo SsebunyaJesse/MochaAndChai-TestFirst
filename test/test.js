9cd8dd674f8d7a2aeca54957d2709db1e6f507e3
SsebunyaJesse <71438169+SsebunyaJesse@users.noreply.github.com>
2022-05-22 23:42:39 +0300

    First commit Including the TDD cycle 1

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const M = require('../multiply')
+
+// Test first TDD cycle 1
+describe('Testing the Multiply function',function(){
+    it('this is a multiply test',function (){
+        assert.equal((1*1),1,'not true...just lies')
+    })
+})
